e4b3ccae871b5be87f0af211a3f16935e06c160a
Breval <breval2000@live.fr>
2025-01-29 23:00:46 +0100

    feat: add config management and screenshot capture
    - Introduced new commands for capturing game regions and managing app configuration.
    - Added functionality to get and update application settings using a mutex for thread safety.
    - Reorganized module imports for better structure.

--- src-tauri/src/core/mod.rs
+++ src-tauri/src/core/mod.rs
@@ -1,11 +1,14 @@
-mod state;
+mod config;
 mod screenshot;
+mod state;
 // mod actions;
 
-use tauri::State;
 use std::sync::{Arc, Mutex};
+use tauri::State;
 
+pub use screenshot::{capture_region, CaptureError, ScreenRegion};
 pub use state::AppState;
+pub use config::AppConfig;
 // pub use actions::{Action, ActionHandler};
 
 #[tauri::command]
@@ -27,6 +30,33 @@ pub fn stop_bot(state: State<'_, AppState>) -> Result<(), String> {
 #[tauri::command]
 pub fn take_screenshot(state: State<'_, AppState>) -> Result<(), String> {
     println!("Taking screenshot");
+    Ok(())
+}
+
+#[tauri::command]
+pub fn capture_game_region() -> Result<(), String> {
+    let region: ScreenRegion = (0, 0, 1920, 1080);
+
+    let image = capture_region(region).map_err(|e| e.to_string())?;
+
+    image.save("image.png").unwrap();
+    Ok(())
+}
+
+#[tauri::command]
+async fn get_config(state: State<'_, Arc<Mutex<AppConfig>>>) -> Result<AppConfig, String> {
+    let config = state.lock().unwrap().clone();
+    Ok(config)
+}
+
+#[tauri::command]
+async fn update_config(
+    new_config: AppConfig,
+    state: State<'_, Arc<Mutex<AppConfig>>>,
+) -> Result<(), String> {
+    let mut config = state.lock().unwrap();
+    *config = new_config;
+    config.save().map_err(|e| e.to_string())?;
     Ok(())
 }
 
