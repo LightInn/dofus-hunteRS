988916ce5814999a7bf8923d1ba8356bac33b7f2
Breval <breval2000@live.fr>
2025-01-29 23:00:56 +0100

    feat: add game region capture functionality
    - Included a new function to capture game regions.
    - Updated the config loading process for better management.
    - Enhanced the command handler to support the new feature.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,6 @@
 mod core;
 
-use core::{start_bot, stop_bot,take_screenshot};
+use core::{start_bot, stop_bot,take_screenshot,capture_game_region};
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
@@ -10,10 +10,16 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+
+    let config = load_config("settings.json").expect("failed to load config");
+
+    let config = Arc::new(Mutex::new(config));
+
     tauri::Builder::default()
         .manage(core::AppState::default())
+        .manage(config)
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet, start_bot, stop_bot,take_screenshot])
+        .invoke_handler(tauri::generate_handler![greet, start_bot, stop_bot,take_screenshot,capture_game_region])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
