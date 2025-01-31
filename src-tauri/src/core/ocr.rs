7f8f9e98304f57fb4702e1b5de3c51f157e043f0
Breval <breval2000@live.fr>
2025-01-31 22:52:58 +0100

    feat: Improve hint parsing logic
    - Updated conditions for valid hints to require longer lines.
    - Removed uppercase words from hints and trimmed leading spaces.
    - Added debug print statements for extracted text and parsed info.

--- src-tauri/src/core/ocr.rs
+++ src-tauri/src/core/ocr.rs
@@ -67,8 +67,8 @@ fn parse_hunt_panel_text(text: Vec<String>) -> HuntPanelInfos {
         }
 
         // si len > 2  +  at least 2 words  + tout n'est pas en majuscule (sauf espace) + un seul chiffre max + pas de parenthèse alors c'est un indice potentiel
-        if line.len() > 2
-            && line.split_whitespace().count() >= 2
+        if line.len() > 3
+            // && line.split_whitespace().count() >= 2
             && !line.chars().all(|c| c.is_uppercase() || c.is_whitespace())
             && line.chars().filter(|c| c.is_digit(10)).count() <= 1
             && !line.contains("(")
@@ -81,12 +81,24 @@ fn parse_hunt_panel_text(text: Vec<String>) -> HuntPanelInfos {
 
     // Traitement de l'indice
     if let Some(hint) = hint_validated {
+        // on supprime le premier caractère
+
         infos.current_hint = hint
             .chars()
             .skip(1) // Supprime le premier caractère
             .collect::<String>()
             .trim_start() // Supprime les espaces initiaux restants
             .to_string();
+
+        // on supprimes les mots entiers lorsqu'ils sont en majuscule
+        let mut hint_words = infos.current_hint.split_whitespace().collect::<Vec<&str>>();
+        let mut hint_words_filtered = Vec::new();
+        for word in hint_words.iter() {
+            if !word.chars().all(|c| c.is_uppercase()) {
+                hint_words_filtered.push(*word);
+            }
+        }
+        infos.current_hint = hint_words_filtered.join(" ");
     }
 
     infos
@@ -96,6 +108,8 @@ fn parse_hunt_panel_text(text: Vec<String>) -> HuntPanelInfos {
 
 pub fn ocr_hunt_panel(image: &DynamicImage) -> Result<HuntPanelInfos, Box<dyn Error>> {
     let extracted_text = ocr(image)?;
+
+    println!("{:?}", extracted_text);
 
     let infos = parse_hunt_panel_text(extracted_text.clone());
 
